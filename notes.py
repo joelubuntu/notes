7f2680133090754976892003e8d08416422068e6
joelubuntu <72544077+joelubuntu@users.noreply.github.com>
2023-01-28 23:53:35 +0530

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,4 +1,4 @@
-import os
+import os , plyer , platform , subprocess
 from tkinter import *
 
 main_menu = Tk()
@@ -6,3 +6,11 @@ main_menu = Tk()
 Label(main_menu,text="Notes Title: ").grid(row=0,column=0)
 
 main_menu.mainloop()
+
+
+#reminders
+if platform.system() == "Windows":
+	Title = ""
+	Message = ""
+elif platform.system() == "Linux":
+	subprocess.call(['notify-send',title,message])
